631a13272b5a4b0da8d4bcf7420eea5122d39360
Alex Imbrea <alex@kappaml.com>
2025-05-10 19:11:29 +0200

    Bump version

--- kappaml/__init__.py
+++ kappaml/__init__.py
@@ -2,16 +2,6 @@
 KappaML - A Python package for machine learning model evaluation and monitoring.
 """
 
-from .kappaml import (
-    evaluate_model,
-    evaluate_model_with_metrics,
-    evaluate_model_with_plots,
-    evaluate_model_with_metrics_and_plots,
-    evaluate_model_with_metrics_and_plots_and_save,
-    evaluate_model_with_metrics_and_save,
-    evaluate_model_with_plots_and_save,
-    evaluate_model_and_save,
-)
+from .kappaml import KappaML
 
-__version__ = "0.1.0"
-__author__ = "Alex Imbrea" 
+__all__ = ["KappaML"]
